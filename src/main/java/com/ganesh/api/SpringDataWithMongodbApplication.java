4a64a1dbd6613f7e6007344aeb4d95343cdf3172
Ganeshk750 <ganesh9ot@gmail.com>
2021-01-17 19:47:04 +0530

    Initial commit

---
+++ src/main/java/com/ganesh/api/SpringDataWithMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.ganesh.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWithMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataWithMongodbApplication.class, args);
+	}
+
+}
